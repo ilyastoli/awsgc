84a6cfce3ba6f667dcd55653d6dab1ee03e325ac
Ilya Stoliar <52413756+ilyastoli@users.noreply.github.com>
2021-07-29 07:46:35 +0300

    Add AWS and Grafana urls

---
+++ awsgc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+aws_doc_url = "https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/aws-services-cloudwatch-metrics.html"
+grafana_cloudwatch_url = "https://raw.githubusercontent.com/grafana/grafana/main/pkg/tsdb/cloudwatch/metric_find_query.go"
