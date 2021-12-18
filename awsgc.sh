7e4dd84c379d4ea79772052cd77fe86e34a42193
ilyaintezer <ilya@intezer.com>
2021-12-18 18:20:07 +0200

    Grafana team moved the metrics to a different file

--- awsgc.sh
+++ awsgc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 aws_doc_url="https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/aws-services-cloudwatch-metrics.html"
-grafana_cloudwatch_url="https://raw.githubusercontent.com/grafana/grafana/main/pkg/tsdb/cloudwatch/metric_find_query.go"
+grafana_cloudwatch_url="https://raw.githubusercontent.com/grafana/grafana/main/pkg/tsdb/cloudwatch/metrics.go"
 
 aws=$(curl -s ${aws_doc_url} | grep "AWS/" | awk -F"\"code\">" '{print $2}' | awk -F"</code>" '{print $1}' | sort | uniq)
 grafana=$(curl -s ${grafana_cloudwatch_url} | grep "AWS/" | awk -F"\"" '{print $2}' | sort | uniq)
